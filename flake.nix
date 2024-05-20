bda6339aac47073ae6113d406e83627d97fe8bf2
Julian Theis <julian.theis@posteo.de>
2024-05-20 14:17:16 +0200

    Remove nodejs dependency

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     devShells.x86_64-linux.default = with import nixpkgs { system = "x86_64-linux"; };
       mkShell {
         packages = [
-          nodejs_21
           yaml-language-server
           zola
         ];
