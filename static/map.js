5d854b74b3f5a18bbda01ec8e7cb2821708b2ca8
Julian <12139995+jvytee@users.noreply.github.com>
2025-03-23 18:11:03 -0600

    Add Travel Overview (#18)
    * Start constructing overview map
    
    * Compute actual center
    
    * Create basic template for overview page
    
    * Include leaflet stylesheet
    
    * Remove unnecessary event handler
    
    * Set correct title
    
    * Reduce map height
    
    * Automatically fit map to markers
    
    * Render page content
    
    * Remove unnecessary folder
    
    * Bind popups to markers
    
    * Link travel overview in navigation
    
    * Add dates, titles, links to popups
    
    * Add usage hint, make fonts consistent
    
    * Use Inter fonts in whole leaflet container
    
    * Remove unnecessary folder
    
    ---------
    
    Co-authored-by: jvytee <jvytee@posteo.org>

--- static/map.js
+++ static/map.js
@@ -25,3 +25,23 @@ export const initMap = (location, tileUrl, tileAttribution) => {
         }
     };
 };
+
+export const initOverviewMap = (posts, tileUrl, tileAttribution) => {
+    const locations = posts.map((post) => post.location);
+    const upper = locations.reduce((acc, current) => [Math.max(acc[0], current[0]), Math.max(acc[1], current[1])], posts[0].location)
+    const lower = locations.reduce((acc, current) => [Math.min(acc[0], current[0]), Math.min(acc[1], current[1])], posts[0].location)
+
+    const mapDiv = document.getElementById("overviewMap");
+    const mapObj = map(mapDiv).fitBounds([lower, upper]);
+
+    tileLayer(
+        tileUrl,
+        { maxZoom: 18, attribution: tileAttribution }
+    ).addTo(mapObj);
+
+    posts.forEach((post) => {
+        marker(post.location)
+            .addTo(mapObj)
+            .bindPopup(`${post.date}<br><a href=${post.path}>${post.title}</a>`);
+    });
+};
